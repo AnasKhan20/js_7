67b82c3439debd97c732f76e17c5d2c330e9171d
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-10-10 00:31:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// CHAPTER 46,48(Events)
+
+// Question 1
+
+// function pop(){
+//     alert("helllo user")
+// }
+
+// Question 6
+
+    // var anc = document.getElementById("web")
+    // anc.target = "_blank"
+
+    // Question 7
+// function get(){
+//     alert("hello")
+// }
+
+
+
+// CHAPTER 49,50
+
+// Question 1
+
+// function get(){
+
+//     var user = document.getElementById("userinput")
+//     console.log(user.value)
+// }
